1d63b446d2b130f0db71de36a681c69ed512795d
Luis Guilherme Cipriani <cipriani.luisguilherme@gmail.com>
2023-09-11 15:25:15 -0300

    Cria ícone de carrinho com quantidade de ítens

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,16 +1,86 @@
-import { View, Text, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { View, Text, StyleSheet, SafeAreaView, FlatList, TouchableOpacity } from "react-native";
 
-export default function Home(){
+import {Feather} from '@expo/vector-icons';
 
-    return(
-        <View style={styles.container}>
-            <Text>Página Home</Text>
-        </View>
+export default function Home() {
+
+    const [products, setProducts] = useState([
+        {
+            id: '1',
+            name: "Coca cola",
+            price: 19.90
+        },
+        {
+            id: '2',
+            name: "Chocolate",
+            price: 6.50
+        },
+        {
+            id: '4',
+            name: "Queijo 500g",
+            price: 15
+        },
+        {
+            id: '5',
+            name: "Batata frita",
+            price: 23.90
+        },
+        {
+            id: '6',
+            name: "Guarana lata",
+            price: 6.00
+        },
+    ])
+
+    return (
+        <SafeAreaView style={styles.container}>
+            <View style={styles.cartContent}>
+                <Text style={styles.title}>Lista de produtos</Text>
+                
+                <TouchableOpacity style={styles.cartButton}>
+                    <View style={styles.dot}>
+                        <Text style={styles.dotText}>5</Text>
+                    </View>
+                    <Feather name="shopping-cart" size={30} color="#000"/>
+                </TouchableOpacity>
+
+            </View>
+        </SafeAreaView>
     )
 }
 
 const styles = StyleSheet.create({
-    container:{
-        flex: 1
+    container: {
+        flex: 1,
+        backgroundColor: "#FAFAFA",
+        paddingEnd: 14,
+        paddingStart: 14,
+    },
+    cartContent: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginTop: 24,
+        marginBottom: 24,
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+    },
+    dot: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: 'red',
+        width: 20,
+        height: 20,
+        borderRadius: 12,
+        position: 'absolute',
+        zIndex: 99,
+        bottom: -2,
+        left: -4,
+    },
+    dotText: {
+        fontSize: 12,
     }
 })
