c83cb3ae2a6e0b5a25e7870f95bf21ae5b5808fa
Luis Guilherme Cipriani <cipriani.luisguilherme@gmail.com>
2023-09-11 18:39:42 -0300

    Calcula total do carrinho de compras

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,37 +1,44 @@
-import React, { useContext } from "react";
-import { View, Text, StyleSheet, FlatList } from "react-native";
-import { CartContext } from '../../contexts/CartContext';
-import CardItem from "../../components/CardItem";
+import { useContext } from 'react'
+import { View, Text, StyleSheet, FlatList, } from 'react-native'
+import { CartContext } from '../../contexts/CartContext'
+import CardItem from '../../components/CardItem'
 
-export default function Cart() {
+export default function Cart(){
+  const { cart, addItemCart, removeItemCart, total } = useContext(CartContext);
 
-    const { cart, addItemCart, removeItemCart } = useContext(CartContext);
 
-    return (
-        <View style={styles.container}>
-            <FlatList
-                data={cart}
-                showsVerticalScrollIndicator={false}
-                keyExtractor={(item) => String(item.id)}
-                ListEmptyComponent={() => <Text>Nenhum ítem no carrinho...</Text>}
-                renderItem={({ item }) => (
-                    <CardItem
-                        data={item}
-                        addAmount={() => addItemCart(item)}
-                        removeAmount={() => removeItemCart(item)}
-                    />
-                )}
-            />
-        </View>
-    )
+
+  return(
+    <View style={stlyes.container}>
+      <FlatList
+        data={cart}
+        showsVerticalScrollIndicator={false}
+        keyExtractor={ (item) => String(item.id) }
+        ListEmptyComponent={ () => <Text>Nenhum item no carrinho...</Text>}
+        renderItem={ ({ item }) => (
+          <CardItem
+            data={item}
+            addAmount={ () => addItemCart(item) }
+            removeAmount={ () => removeItemCart(item) }
+          />
+        )}
+        ListFooterComponent={ () => <Text style={stlyes.total}>Total: R$ {total}</Text> }
+      />
+    </View>
+  )
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#FAFAFA",
-        paddingStart: 14,
-        paddingEnd: 14,
-        paddingTop: 14,
-    }
+const stlyes = StyleSheet.create({
+  container:{
+    flex:1,
+    backgroundColor: '#FAFAFA',
+    paddingStart: 14, 
+    paddingEnd: 14,
+    paddingTop: 14,
+  },
+  total:{
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 24,
+  }
 })
